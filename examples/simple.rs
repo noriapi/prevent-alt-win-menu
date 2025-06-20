52ca2daf01862ffd6b472678838f587d47ab06b6
noriapi <70106808+noriapi@users.noreply.github.com>
2025-06-20 12:51:31 +0900

    fix!: remove `join`

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,13 @@
+use std::{thread, time::Duration};
+
 fn main() {
     #[cfg(feature = "log")]
     colog::init();
 
-    let handles = prevent_alt_win_menu::start(Default::default()).unwrap();
-    handles.join().unwrap();
+    prevent_alt_win_menu::start(Default::default()).unwrap();
+
+    // your main code ...
+    loop {
+        thread::sleep(Duration::from_secs(10))
+    }
 }
