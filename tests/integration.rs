76033ca461fa77d340a6a0d29c0a1dc8e6b5e407
noriapi <70106808+noriapi@users.noreply.github.com>
2025-06-21 12:57:21 +0900

    test: add tests for start menu behaviour (#14)
    * test: add tests for start menu behaviour
    
    * ci: skip integration tests for now

---
+++ tests/integration.rs
@@ -0,0 +1,71 @@
+use std::{thread, time::Duration};
+
+use uiautomation::{
+    UIAutomation, UIElement, UIMatcher,
+    errors::{ERR_NOTFOUND, ERR_TIMEOUT},
+};
+
+#[test]
+fn show_start_menu() {
+    let automation = UIAutomation::new().unwrap();
+    let matcher = start_menu_matcher(&automation);
+
+    assert_no_match(&matcher);
+
+    send_win(&automation);
+
+    assert_match(&matcher);
+
+    send_win(&automation);
+
+    assert_no_match(&matcher);
+}
+
+#[test]
+fn prevent_to_show_start_menu() {
+    prevent_alt_win_menu::start(Default::default()).unwrap();
+
+    let automation = UIAutomation::new().unwrap();
+    let matcher = start_menu_matcher(&automation);
+
+    assert_no_match(&matcher);
+
+    send_win(&automation);
+
+    assert_no_match(&matcher);
+}
+
+fn send_win(automation: &UIAutomation) {
+    let root = automation.get_root_element().unwrap();
+    root.send_keys("{Win}", 0).unwrap();
+}
+
+#[track_caller]
+fn assert_no_match(matcher: &UIMatcher) {
+    thread::sleep(Duration::from_millis(500));
+
+    assert!(matches!(
+        matcher.find_first().err().map(|e| e.code()),
+        Some(ERR_NOTFOUND | ERR_TIMEOUT)
+    ));
+}
+
+#[track_caller]
+fn assert_match(matcher: &UIMatcher) {
+    assert!(matcher.find_first().is_ok());
+}
+
+fn start_menu_matcher(automation: &UIAutomation) -> UIMatcher {
+    let root = automation.get_root_element().unwrap();
+
+    automation
+        .create_matcher()
+        .from_ref(&root)
+        .timeout(500)
+        .depth(2)
+        .filter_fn(Box::new(|e: &UIElement| {
+            let name = e.get_name()?;
+            Ok(name == "Start" || name == "スタート")
+        }))
+        .classname("Windows.UI.Core.CoreWindow")
+}
